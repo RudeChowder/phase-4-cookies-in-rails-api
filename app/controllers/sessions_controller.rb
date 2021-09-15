01a4c923c487bb97f13690e98a457777e060c94e
Stephen Crane <scrane88@gmail.com>
2021-09-15 14:26:49 -0400

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
+require "pry"
 class SessionsController < ApplicationController
   
   def index
+    binding.pry
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
